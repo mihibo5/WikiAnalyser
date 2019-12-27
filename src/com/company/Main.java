4732e3de41aa0fbeff26b592bae1204a5b4e802e
mihibo5 <miha.bo@hotmail.com>
2019-12-27 19:55:25 +0100

    Now using StringBuilder to optimize

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -28,7 +29,7 @@ public class Main {
 
         if (args.length > 0) {
             //all we do in main function is start the process
-            Main.start(BASE_URL + args[0]);
+            Main.start(BASE_URL + args[0], false);
         }
         else {
             throw new Exception("URL required!");
@@ -44,7 +45,7 @@ public class Main {
 
     }
 
-    private static void start(String startUrl) throws IOException {
+    private static void start(String startUrl, boolean debug) throws IOException {
         /*
         now we should loop through all elements for which we have two options:
             -recursively
@@ -64,8 +65,9 @@ public class Main {
         while (queue.size() > 0) {
             String currentUrl = queue.remove();
             System.out.println(index + ": " + currentUrl + ", queue size: " + queue.size());
+            if (debug) System.out.println(printRuntimeUsage());
 
-            String result = httpRequest(currentUrl);
+            StringBuilder result = httpRequest(currentUrl);
             if (result != null) {
                 String title = extractTitle(result);
                 List<String> urls = extractUrls(extractContent(result));
@@ -87,7 +89,7 @@ public class Main {
         }
     }
 
-    private static String httpRequest(String url) {
+    private static StringBuilder httpRequest(String url) {
         try {
             URL requestURL = new URL(url);
             URLConnection requestConnection = requestURL.openConnection();
@@ -101,28 +103,28 @@ public class Main {
             }
             in.close();
 
-            return result.toString();
+            return result;
         } catch (IOException e) {
             return null;
         }
     }
 
-    private static String extractTitle(String content) {
+    private static String extractTitle(StringBuilder content) {
         String title = content.substring(content.indexOf("<title>") + 7, content.indexOf("</title>"));
         int wikiIndex = title.indexOf(" - Wikipedia");
         if (wikiIndex == -1) wikiIndex = title.length();
         return title.substring(0, wikiIndex);
     }
 
-    private static String extractContent(String content) {
+    private static StringBuilder extractContent(StringBuilder content) {
         int start = Math.max(0, content.indexOf("<div id=\"content\" class=\"mw-body\" role=\"main\">"));
         int end = content.indexOf("<span class=\"mw-headline\" id=\"References\">References</span>");
         if (end == -1) end = content.length() - 1;
 
-        return content.substring(start, end);
+        return new StringBuilder(content.substring(start, end));
     }
 
-    private static List<String> extractUrls(String content) {
+    private static List<String> extractUrls(StringBuilder content) {
         /*
          Here we extract all urls from a specific site.
          Notes:
@@ -132,9 +134,9 @@ public class Main {
 
         List<String> urls = new ArrayList<>();
 
-        while (content.contains("<a href=")) {
+        while (content.toString().contains("<a href=")) {
             //move to the first found url
-            content = content.substring(content.indexOf("<a href="));
+            content = new StringBuilder(content.substring(content.indexOf("<a href=")));
             String sub = content.substring(content.indexOf("<a href="), content.indexOf("</a>") + 4);
 
             //check if the link is correct
@@ -159,9 +161,27 @@ public class Main {
             }
 
             //move past first found url
-            content = content.substring(content.indexOf("</a>") + 4);
+            content = new StringBuilder(content.substring(content.indexOf("</a>") + 4));
         }
 
         return urls;
+    }
+
+    private static String printRuntimeUsage() {
+        Runtime runtime = Runtime.getRuntime();
+
+        NumberFormat format = NumberFormat.getInstance();
+
+        StringBuilder sb = new StringBuilder();
+        long maxMemory = runtime.maxMemory();
+        long allocatedMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+
+        sb.append("free memory: ").append(format.format(freeMemory)).append(" - ");
+        sb.append("allocated memory: ").append(format.format(allocatedMemory)).append(" - ");
+        sb.append("max memory: ").append(format.format(maxMemory)).append(" - ");
+        sb.append("total free memory: ").append(format.format((freeMemory + (maxMemory - allocatedMemory))));
+
+        return sb.toString();
     }
 }
